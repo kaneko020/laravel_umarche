43729621dc0367a2f53aa1c6321d50ffb22c56bd
laravel_umarche <n-kaneko@jutjoy.com>
2022-12-13 10:47:23 +0900

    classBase

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,8 +1,9 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
-コンポーネントテスト1
+    コンポーネントテスト1
 
-<x-tests.card title="タイトル1" content="本文1" :message="$message" />
-<x-tests.card title="タイトル" />
-<x-tests.card title="CSSを変更したい" class="bg-red-300" />
+    <x-tests.card title="タイトル1" content="本文1" :message="$message" />
+    <x-tests.card title="タイトル" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
+
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,32 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-tests.app>
+    <x-slot name="header">ヘッダー2</x-slot>
+    コンポーネントテスト2
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <header>
-            ヘッダー1
-        </header>
-        <div class="font-sans text-gray-900 antialiased">
-            コンポーネントテスト1
-            <div class="border-2 shadow-md w-1/4 p-2">
-                <div>タイトル1</div>
-                <div>画像</div>
-                <div>本文1</div>
-            </div>
-        </div>
-    </body>
-</html>
+    <x-test-class-base classBaseMessage="メッセージ" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージ" defaultMessage="初期値から変更しています" />
+</x-tests.app>
