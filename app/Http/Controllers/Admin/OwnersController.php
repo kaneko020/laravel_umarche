129bfa36622d5e910a6512727398fd95aa8a10c2
laravel_umarche <n-kaneko@jutjoy.com>
2022-12-14 15:32:43 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -78,7 +78,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました');
+        ->with([
+            'message' => 'オーナー登録を実施しました',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -122,7 +125,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しました');
+        ->with([
+            'message' => 'オーナー情報を更新しました',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -133,6 +139,32 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete(); //ソフトデリート
+        
+        return redirect()
+        ->route('admin.owners.index')
+        ->with([
+            'message' => 'オーナー情報を削除しました',
+            'status' => 'alert'
+        ]);
+
+        // dd('削除処理');
+    }
+
+    public function expiredOwnerIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()
+        ->route('admin.expired-owners.index')
+        ->with([
+            'message' => 'オーナー情報を完全に削除しました',
+            'status' => 'alert'
+        ]);
     }
 }
