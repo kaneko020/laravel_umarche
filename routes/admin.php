f8e91c6c8b6040385d1d33cf9e043314ba03acba
laravel_umarche <n-kaneko@jutjoy.com>
2022-12-15 11:12:08 +0900

    middleware abort

--- routes/admin.php
+++ routes/admin.php
@@ -31,10 +31,8 @@ Route::resource('owners', OwnersController::class)
 Route::prefix('expired-owners')
 ->middleware('auth:admin')
 ->group(function(){
-    Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])
-    ->name('expired-owners.index');
-    Route::post('destroy/{owner}', [OwnersController::class, 'expiredOwnerDestroy'])
-    ->name('expired-owners.destroy');
+    Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
+    Route::post('destroy/{owner}', [OwnersController::class, 'expiredOwnerDestroy'])->name('expired-owners.destroy');
 });
 
 Route::get('/dashboard', function () {
